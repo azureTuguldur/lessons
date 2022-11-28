013da383033952f44190e53be6efd18a9f3897f1
Evening <evening@admins-iMac.local>
2022-11-28 17:24:55 +0800

    sdasdadsadas

---
+++ html/jsbasic/js1.js
@@ -0,0 +1,46 @@
+        // 1.
+        // Commends can make code readable
+        // 2.
+        // Java script lesson begin
+        // 3.
+        // Commends
+        // can 
+        // make 
+        // code
+        // readable
+        // 4.
+        let number = 88; 
+        let string = "Lam";
+        Boolean(10 > 9)
+        let job = 1;
+        job = null;
+        undefined
+        // 5.
+        console.log(number);
+        console.log(string);
+        console.log(Boolean);
+        console.log(job);
+        console.log(undefined);
+        // 6.
+        let 
+        // 7.
+        s
+        let dd
+        // 8.
+        let first_name = "Tuguldur";
+        let last_name = "Baynmunkh";     
+        let marital_status = "single";
+        let country_ = "Mongolia";
+        let age_ = "19";
+        // 9.
+        let firstname = "Tuguldur"; let lastname = "Baynmunkh"; let maritalstatus = "single"; let country = "Mongolia" ; let age = "19";
+        // 10.
+        let myage   = 19 ;
+        let yourage = "?" ;
+        // 11.
+        // gurvaljin perimeter ol
+        // 12.
+        //  toirogin talbai ol
+        // 13.
+        // m = y2-y1/x2-x1 ol
+        // 14.
